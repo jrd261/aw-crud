a8c7b533b55bedfc3fb955245ab42bbfbd9d3c1a
James Dalessio <jrd261@gmail.com>
2015-06-02 16:57:20 -0400

    Move back to argument free calls. Add prehook.

--- index.js
+++ index.js
@@ -11,74 +11,66 @@ var Controller = function (options) {
     this.options = options;
   };
 
+  this.prehook = function () {};
+
   this.create = function (request, response, next) {
     var context = new Context(request, response, 'create');
     return Promise.resolve()
+      .then(controller.prehook.bind(context))
       .then(controller.creator.bind(context))
-      .then(function (resource) {
-        context.resource = resource;
-      })
-      .then(function () { 
-        controller.updater.call(context, context.resource); 
-      })
-      .then(function () {
-        controller.writer.call(context, context.resource);
-      })
-      .then(function () {
-        controller.responder.call(context, context.resource);
-      })
+      .then(function (resource) { context.resource = resource; })
+      .then(controller.updater.bind(context)) 
+      .then(controller.writer.bind(context))
+      .then(controller.responder.bind(context))
       .catch(next);  
   };
 
   this.read = function (request, response, next) {
     var context = new Context(request, response, 'read');
     return Promise.resolve()
+      .then(controller.prehook.bind(context))
       .then(controller.reader.bind(context))
-      .then(function (resource) {
-        context.resource = resource;
-      })
-      .then(function () {
-        controller.responder.call(context, context.resource);
-      })
+      .then(function (resource) { context.resource = resource; })
+      .then(controller.responder.bind(context))
       .catch(next);
   };
 
   this.update = function (request, response, next) {
     var context = new Context(request, response, 'update');
     return Promise.resolve()
+      .then(controller.prehook.bind(context))
       .then(controller.reader.bind(context))
-      .then(function (resource) {
-        context.resource = resource;
-      })
-      .then(function () { 
-        controller.updater.call(context, context.resource);
-      })
-      .then(function () {
-        controller.writer.call(context, context.resource);
-      })
-      .then(function () {
-        controller.responder.call(context, context.resource);
-      })
+      .then(function (resource) { context.resource = resource; })
+      .then(controller.updater.bind(context))
+      .then(controller.writer.bind(context))
+      .then(controller.responder.bind(context))
       .catch(next);
   };
 
-  this.delete = function (request, reponse, next) {
+  this.delete = function (request, response, next) {
     var context = new Context(request, response, 'delete');
     return Promise.resolve()
+      .then(controller.prehook.bind(context))
       .then(controller.reader.bind(context))
-      .then(function (resource) {
-        context.resource = resource; 
-      })
-      .then(function () {
-        controller.deleter.call(context, context.resource);
-      })
+      .then(function (resource) { context.resource = resource; })
+      .then(controller.deleter.bind(context))
       .then(controller.responder.bind(context))
       .catch(next);    
   };
 
+  this.list = function (request, response, next) {
+    var context = new Context(request, response, 'list');
+    return Promise.resolve()
+      .then(controller.prehook.bind(context))
+      .then(controller.lister.bind(context))
+      .then(function (resource) { context.resource = resource; })
+      .then(controller.responder.bind(context))
+      .catch(next);
+  };
+
 };
 
-Controller.prototype.responder = function (resource) {
+Controller.prototype.responder = function () {
   if (this.action === 'create') {
     this.response.status(201);
   } else {
@@ -87,7 +79,7 @@ Controller.prototype.responder = function (resource) {
   if (this.action === 'delete') {
     this.response.send();
   } else {
-    this.response.send(resource);
+    this.response.send(this.resource);
   }
 };
 
@@ -96,5 +88,6 @@ Controller.prototype.updater = function () {};
 Controller.prototype.reader = function () {};
 Controller.prototype.writer = function () {};
 Controller.prototype.deleter = function () {};
+Controller.prototype.lister = function () {};
 
 module.exports.Controller = Controller;
